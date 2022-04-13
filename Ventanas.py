5e7b7128feac447c02f9735b787e9ba453c8319c
Your Name <nasuarezro@unal.edu.co>
2022-04-12 22:53:42 -0500

    Todos menus funcionando en menus.py

--- Ventanas.py
+++ Ventanas.py
@@ -37,19 +37,35 @@ class VentanaPrincipal(tk.Tk):
 
         bar_menu = tk.Menu()  ## Crear barra de menú
         self.config(menu=bar_menu)  ## Insertar la barra de menús al la principal  (Se deben añadir menus, sino no se ve)
-        menu_archivo = tk.Menu(bar_menu, tearoff=False)
+
+        menu_archivo = tk.Menu(bar_menu, tearoff=False) ## Creacion menú archivo
+        menu_archivo, bar_menu = menuarchivo(menu_archivo, bar_menu) ##Funcion que añade submenus, etc
+        bar_menu.add_cascade(menu=menu_archivo, label="Archivo") ##Añado a la barra de menus
 
 
-        menu_archivo, bar_menu = menuarchivo(menu_archivo, bar_menu)
+        menu_editar = tk.Menu(bar_menu, tearoff=False) ## Creacion menú editar
+        menu_editar, bar_menu = menueditar(menu_editar, bar_menu) ##Funcion que añade submenus, etc
+        bar_menu.add_cascade(menu=menu_editar, label="Editar") ##Añado a la barra de menus
 
+        menu_analizar = tk.Menu(bar_menu, tearoff=False)  ## Creacion menú analizar
+        menu_analizar, bar_menu = menuanalizar(menu_analizar, bar_menu)  ##Funcion que añade submenus, etc
+        bar_menu.add_cascade(menu=menu_analizar, label="Analizar")  ##Añado a la barra de menus
 
-        bar_menu.add_cascade(menu=menu_archivo, label="Archivo")
+        menu_herramienta = tk.Menu(bar_menu, tearoff=False)  ## Creacion menú herramienta
+        menu_herramienta, bar_menu = menuherramienta(menu_herramienta, bar_menu)  ##Funcion que añade submenus, etc
+        bar_menu.add_cascade(menu=menu_herramienta, label="Herramienta")  ##Añado a la barra de menus
 
+        menu_aplicacion = tk.Menu(bar_menu, tearoff=False)  ## Creacion menú aplicacion
+        menu_aplicacion, bar_menu = menuaplicacion(menu_aplicacion, bar_menu)  ##Funcion que añade submenus, etc
+        bar_menu.add_cascade(menu=menu_aplicacion, label="Aplicación")  ##Añado a la barra de menus
 
+        menu_ventana = tk.Menu(bar_menu, tearoff=False)  ## Creacion menú ventana
+        menu_ventana, bar_menu = menuventana(menu_ventana, bar_menu)  ##Funcion que añade submenus, etc
+        bar_menu.add_cascade(menu=menu_ventana, label="Ventana")  ##Añado a la barra de menus
 
-
-
-
+        menu_ayuda = tk.Menu(bar_menu, tearoff=False)  ## Creacion menú ayuda
+        menu_ayuda, bar_menu = menuayuda(menu_ayuda, bar_menu)  ##Funcion que añade submenus, etc
+        bar_menu.add_cascade(menu=menu_ayuda, label="Ayuda")  ##Añado a la barra de menus
 
     def abrir_ventana_secundaria(self):
         if not VentanaSecundaria.en_uso:
--- menus.py
+++ menus.py
@@ -5,10 +5,8 @@ def archivo_nuevo_presionado():
     print("¡Has presionado para crear un nuevo archivo!")
 
 
-def menuarchivo(menu_archivo, bar_menu):
-    menu_archivo = tk.Menu(bar_menu, tearoff=False)
-
-    sub_menu_archivo_nuevo = tk.Menu(menu_archivo, tearoff=False)
+def menuarchivo(menu, bar_menu):
+    sub_menu_archivo_nuevo = tk.Menu(menu, tearoff=False)
     sub_menu_archivo_nuevo.add_command(
         label="Personalizado",
         ## accelerator="Ctrl+N",
@@ -19,25 +17,25 @@ def menuarchivo(menu_archivo, bar_menu):
         ## accelerator="Ctrl+N",
         command=archivo_nuevo_presionado
     )
-    menu_archivo.add_cascade(menu=sub_menu_archivo_nuevo, label="Nuevo grafo")
+    menu.add_cascade(menu=sub_menu_archivo_nuevo, label="Nuevo grafo")
 
-    menu_archivo.add_command(
+    menu.add_command(
         label="Abrir",
         ## accelerator="Ctrl+N",
         command=archivo_nuevo_presionado
     )
-    menu_archivo.add_command(
+    menu.add_command(
         label="Guardar",
         ## accelerator="Ctrl+N",
         command=archivo_nuevo_presionado
     )
-    menu_archivo.add_command(
+    menu.add_command(
         label="Guardar como",
         ## accelerator="Ctrl+N",
         command=archivo_nuevo_presionado
     )
 
-    sub_menu_archivo_exportar = tk.Menu(menu_archivo, tearoff=False)
+    sub_menu_archivo_exportar = tk.Menu(menu, tearoff=False)
     sub_menu_archivo_exportar.add_command(
         label="Excel",
         ## accelerator="Ctrl+N",
@@ -53,25 +51,154 @@ def menuarchivo(menu_archivo, bar_menu):
         ## accelerator="Ctrl+N",
         command=archivo_nuevo_presionado
     )
-    menu_archivo.add_cascade(menu=sub_menu_archivo_exportar, label="Exportar")
+    menu.add_cascade(menu=sub_menu_archivo_exportar, label="Exportar")
 
-    menu_archivo.add_command(
+    menu.add_command(
         label="Importar datos",
         ## accelerator="Ctrl+N",
         command=archivo_nuevo_presionado
     )
-    menu_archivo.add_command(
+    menu.add_command(
         label="Inicio",
         ## accelerator="Ctrl+N",
         command=archivo_nuevo_presionado
     )
-    menu_archivo.add_command(
+    menu.add_command(
         label="Imprimir",
         ## accelerator="Ctrl+N",
         command=archivo_nuevo_presionado
     )
 
 
-    return (menu_archivo, bar_menu)
+    return(menu, bar_menu)
+
+def menueditar(menu, bar_menu):
+    menu.add_command(
+        label="Deshacer",
+        ## accelerator="Ctrl+N",
+        command=archivo_nuevo_presionado
+    )
+    sub_menu_editar_nodo = tk.Menu(menu, tearoff=False)
+    sub_menu_editar_nodo.add_command(
+        label="Agregar",
+        ## accelerator="Ctrl+N",
+        command=archivo_nuevo_presionado
+    )
+    sub_menu_editar_nodo.add_command(
+        label="Editar",
+        ## accelerator="Ctrl+N",
+        command=archivo_nuevo_presionado
+    )
+    sub_menu_editar_nodo.add_command(
+        label="Eliminar",
+        ## accelerator="Ctrl+N",
+        command=archivo_nuevo_presionado
+    )
+    menu.add_cascade(menu=sub_menu_editar_nodo, label="Nodo")
+
+    sub_menu_editar_arco = tk.Menu(menu, tearoff=False)
+    sub_menu_editar_arco.add_command(
+        label="Agregar",
+        ## accelerator="Ctrl+N",
+        command=archivo_nuevo_presionado
+    )
+    sub_menu_editar_arco.add_command(
+        label="Editar",
+        ## accelerator="Ctrl+N",
+        command=archivo_nuevo_presionado
+    )
+    sub_menu_editar_arco.add_command(
+        label="Eliminar",
+        ## accelerator="Ctrl+N",
+        command=archivo_nuevo_presionado
+    )
+    menu.add_cascade(menu=sub_menu_editar_arco, label="Arco")
 
 
+    return(menu, bar_menu)
+
+def menuanalizar(menu, bar_menu):
+    sub_menu_analizar_algoritmo = tk.Menu(menu, tearoff=False)
+    sub_menu_analizar_algoritmo.add_command(
+        label="Algoritmo 1",
+        ## accelerator="Ctrl+N",
+        command=archivo_nuevo_presionado
+    )
+    sub_menu_analizar_algoritmo.add_command(
+        label="Algoritmo 2",
+        ## accelerator="Ctrl+N",
+        command=archivo_nuevo_presionado
+    )
+    sub_menu_analizar_algoritmo.add_command(
+        label="Algoritmo 3",
+        ## accelerator="Ctrl+N",
+        command=archivo_nuevo_presionado
+    )
+    sub_menu_analizar_algoritmo.add_command(
+        label="Algoritmo k",
+        ## accelerator="Ctrl+N",
+        command=archivo_nuevo_presionado
+    )
+    menu.add_cascade(menu=sub_menu_analizar_algoritmo, label="Algoritmo")
+
+    return(menu, bar_menu)
+
+def menuherramienta(menu, bar_menu):
+    menu.add_command(
+        label="Ejecución",
+        ## accelerator="Ctrl+N",
+        command=archivo_nuevo_presionado
+    )
+    return(menu, bar_menu)
+
+def menuaplicacion(menu, bar_menu):
+    sub_menu_aplicacion = tk.Menu(menu, tearoff=False)
+    sub_menu_aplicacion.add_command(
+        label="Aplicación 1",
+        ## accelerator="Ctrl+N",
+        command=archivo_nuevo_presionado
+    )
+    sub_menu_aplicacion.add_command(
+        label="Aplicación 2",
+        ## accelerator="Ctrl+N",
+        command=archivo_nuevo_presionado
+    )
+    sub_menu_aplicacion.add_command(
+        label="Aplicación 3",
+        ## accelerator="Ctrl+N",
+        command=archivo_nuevo_presionado
+    )
+    sub_menu_aplicacion.add_command(
+        label="Aplicación m",
+        ## accelerator="Ctrl+N",
+        command=archivo_nuevo_presionado
+    )
+    menu.add_cascade(menu=sub_menu_aplicacion, label="Algoritmo")
+
+    return(menu, bar_menu)
+
+def menuventana(menu, bar_menu):
+    menu.add_command(
+        label="Gráfica",
+        ## accelerator="Ctrl+N",
+        command=archivo_nuevo_presionado
+    )
+    menu.add_command(
+        label="Tabla",
+        ## accelerator="Ctrl+N",
+        command=archivo_nuevo_presionado
+    )
+    return(menu, bar_menu)
+
+def menuayuda(menu, bar_menu):
+    menu.add_command(
+        label="Ayuda",
+        ## accelerator="Ctrl+N",
+        command=archivo_nuevo_presionado
+    )
+    menu.add_command(
+        label="Acerca de Grafos",
+        ## accelerator="Ctrl+N",
+        command=archivo_nuevo_presionado
+    )
+    return(menu, bar_menu)
