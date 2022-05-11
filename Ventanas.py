62af42f03ccbb0334dc76518a3da91d25bee11c3
Your Name <nasuarezro@unal.edu.co>
2022-05-11 00:10:53 -0500

    Seleccion de grafos

--- Ventanas.py
+++ Ventanas.py
@@ -4,16 +4,15 @@ import networkx as nx
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-from tkinter import messagebox
-
 from menus import *
 
+
 class VentanaPrincipal(tk.Tk):
-    def __init__(self, *args, **kwargs):  ## Queda abierto a n argumentos o n argumentos con identificador
-        super().__init__(*args, **kwargs)  ## Se almacena por herencia el *args **kwargs
+    def __init__(self, *args, **kwargs):  # Queda abierto a n argumentos o n argumentos con identificador
+        super().__init__(*args, **kwargs)  # Se almacena por herencia el *args **kwargs
         white = ttk.Style()
         white.configure('TFrame', background='#ffffff')
-        ## Inicializar objetos para agregar aristas
+        # Inicializar objetos para agregar aristas
         self.label_Add_edge_title = None
         self.label_Add_edge_vertice_o = None
         self.label_Add_edge_vertice_d = None
@@ -23,70 +22,76 @@ class VentanaPrincipal(tk.Tk):
         self.entry_Add_edge_peso = None
         self.button_Add_edge = None
         self.frameAdd_edge = ttk.Frame(self, style="TFrame")
-        ## Inicializar objetos para agregar nodos
+        # Inicializar objetos para agregar nodos
         self.label_Add_node_title = None
         self.label_Add_node = None
         self.entry_Add_node = None
         self.button_Add_node = None
         self.frameAdd_node = ttk.Frame(self, style="TFrame")
-        ## Inicializar objetos para eliminar nodos
+        # Inicializar objetos para eliminar nodos
         self.label_Delete_node_title = None
         self.label_Delete_node = None
         self.entry_Delete_node = None
         self.button_Delete_node = None
         self.frameDelete_node = ttk.Frame(self, style="TFrame")
-        #Inicializar objetos para el "tablero" o donde se hara la figura "Grafo"
+        # Inicializar objetos para el "tablero" o donde se hara la figura "Grafo"
         self.frameFigure = ttk.Frame(self)
         self.figure = None
-        ## CONFIGURACION DE VENTANA
+        # CONFIGURACION DE VENTANA
         self.geometry("1200x700")
         self.title("Ventana principal")
         self.config(bg='#F2B33D')
 
         init = MyDialog(self, "Seleccion")
         print(init.var.get())
-        #Creacion del grafo
-        if(init.var.get()=="Grafo"):
+        # Creacion del grafo
+        if init.var.get() == "Grafo":
             self.G = nx.Graph()
-        elif(init.var.get()=="Grafo Dirigido"):
+        elif init.var.get() == "Grafo Dirigido":
             self.G = nx.DiGraph()
-        elif (init.var.get() =="Multi Grafo"):
+        elif init.var.get() == "Multi Grafo":
             self.G = nx.MultiGraph()
-        elif(init.var.get()=="Multi Grafo Dirigido"):
+        elif init.var.get() == "Multi Grafo Dirigido":
             self.G = nx.MultiDiGraph()
         self.focus_force()
+        
     def init_menubar(self):
-        self.bar_menu = tk.Menu()  ## Crear barra de menú
+        self.bar_menu = tk.Menu()  # Crear barra de menú
         self.config(
-            menu=self.bar_menu)  ## Insertar la barra de menús al la principal  (Se deben añadir menus, sino no se ve)
+            menu=self.bar_menu)  # Insertar la barra de menús al la principal  (Se deben añadir menus, sino no se ve)
 
-        self.menu_archivo = tk.Menu(self.bar_menu, tearoff=False)  ## Creacion menú archivo
-        self.menu_archivo, self.bar_menu = self.menuarchivo(self.menu_archivo, self.bar_menu)  ##Funcion que añade submenus, etc
-        self.bar_menu.add_cascade(menu=self.menu_archivo, label="Archivo")  ##Añado a la barra de menus
+        self.menu_archivo = tk.Menu(self.bar_menu, tearoff=False)  # Creacion menú archivo
+        self.menu_archivo, self.bar_menu = self.menuarchivo(self.menu_archivo, self.bar_menu)  # Función que añade 
+        # submenus, etc 
+        self.bar_menu.add_cascade(menu=self.menu_archivo, label="Archivo")  # Añado a la barra de menus
 
-        self.menu_editar = tk.Menu(self.bar_menu, tearoff=False)  ## Creacion menú editar
-        self.menu_editar, self.bar_menu = self.menueditar(self.menu_editar, self.bar_menu)  ##Funcion que añade submenus, etc
-        self.bar_menu.add_cascade(menu=self.menu_editar, label="Editar")  ##Añado a la barra de menus
+        self.menu_editar = tk.Menu(self.bar_menu, tearoff=False)  # Creacion menú editar
+        self.menu_editar, self.bar_menu = self.menueditar(self.menu_editar, self.bar_menu)  # Función que añade submenus
+        self.bar_menu.add_cascade(menu=self.menu_editar, label="Editar")  # Añado a la barra de menus
 
-        self.menu_analizar = tk.Menu(self.bar_menu, tearoff=False)  ## Creacion menú analizar
-        self.menu_analizar, self.bar_menu = self.menuanalizar(self.menu_analizar, self.bar_menu)  ##Funcion que añade submenus, etc
-        self.bar_menu.add_cascade(menu=self.menu_analizar, label="Analizar")  ##Añado a la barra de menus
+        self.menu_analizar = tk.Menu(self.bar_menu, tearoff=False)  # Creacion menú analizar
+        self.menu_analizar, self.bar_menu = self.menuanalizar(self.menu_analizar, self.bar_menu)  # Función que añade
+        # submenus, etc
+        self.bar_menu.add_cascade(menu=self.menu_analizar, label="Analizar")  # Añado a la barra de menus
 
-        self.menu_herramienta = tk.Menu(self.bar_menu, tearoff=False)  ## Creacion menú herramienta
-        self.menu_herramienta, self.bar_menu = self.menuherramienta(self.menu_herramienta, self.bar_menu)  ##Funcion que añade submenus, etc
-        self.bar_menu.add_cascade(menu=self.menu_herramienta, label="Herramienta")  ##Añado a la barra de menus
+        self.menu_herramienta = tk.Menu(self.bar_menu, tearoff=False)  # Creacion menú herramienta
+        self.menu_herramienta, self.bar_menu = self.menuherramienta(self.menu_herramienta, self.bar_menu)  #Función que
+        # añade submenus, etc
+        self.bar_menu.add_cascade(menu=self.menu_herramienta, label="Herramienta")  # Añado a la barra de menus
 
-        self.menu_aplicacion = tk.Menu(self.bar_menu, tearoff=False)  ## Creacion menú aplicacion
-        self.menu_aplicacion, self.bar_menu = self.menuaplicacion(self.menu_aplicacion, self.bar_menu)  ##Funcion que añade submenus, etc
-        self.bar_menu.add_cascade(menu=self.menu_aplicacion, label="Aplicación")  ##Añado a la barra de menus
+        self.menu_aplicacion = tk.Menu(self.bar_menu, tearoff=False)  # Creacion menú aplicacion
+        self.menu_aplicacion, self.bar_menu = self.menuaplicacion(self.menu_aplicacion, self.bar_menu)  # Función que
+        # añade submenus, etc
+        self.bar_menu.add_cascade(menu=self.menu_aplicacion, label="Aplicación")  # Añado a la barra de menus
 
-        self.menu_ventana = tk.Menu(self.bar_menu, tearoff=False)  ## Creacion menú ventana
-        self.menu_ventana, self.bar_menu = self.menuventana(self.menu_ventana, self.bar_menu)  ##Funcion que añade submenus, etc
-        self.bar_menu.add_cascade(menu=self.menu_ventana, label="Ventana")  ##Añado a la barra de menus
+        self.menu_ventana = tk.Menu(self.bar_menu, tearoff=False)  # Creacion menú ventana
+        self.menu_ventana, self.bar_menu = self.menuventana(self.menu_ventana, self.bar_menu)  # Función que añade
+        # submenus, etc
+        self.bar_menu.add_cascade(menu=self.menu_ventana, label="Ventana")  # Añado a la barra de menus
 
-        self.menu_ayuda = tk.Menu(self.bar_menu, tearoff=False)  ## Creacion menú ayuda
-        self.menu_ayuda, self.bar_menu = self.menuayuda(self.menu_ayuda, self.bar_menu)  ##Funcion que añade submenus, etc
-        self.bar_menu.add_cascade(menu=self.menu_ayuda, label="Ayuda")  ##Añado a la barra de menus
+        self.menu_ayuda = tk.Menu(self.bar_menu, tearoff=False)  # Creacion menú ayuda
+        self.menu_ayuda, self.bar_menu = self.menuayuda(self.menu_ayuda, self.bar_menu)  # Función que añade submenus
+        self.bar_menu.add_cascade(menu=self.menu_ayuda, label="Ayuda")  # Añado a la barra de menus
 
     def init_buttons(self):
         self.frameAdd_edge.grid(row=0, column=0, padx=20, pady=20)
@@ -232,7 +237,7 @@ class VentanaPrincipal(tk.Tk):
 
         self.pos = nx.random_layout(self.G)
 
-        ##self.pos = nx.spring_layout(self.G)
+        #self.pos = nx.spring_layout(self.G)
         nx.draw_networkx(self.G, self.pos, ax=self.ax)
 
         canvas = FigureCanvasTkAgg(self.figure, master=self.frameFigure)
@@ -288,63 +293,63 @@ class VentanaPrincipal(tk.Tk):
         sub_menu_archivo_nuevo = tk.Menu(menu, tearoff=False)
         sub_menu_archivo_nuevo.add_command(
             label="Personalizado",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         sub_menu_archivo_nuevo.add_command(
             label="Aleatorio",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         menu.add_cascade(menu=sub_menu_archivo_nuevo, label="Nuevo grafo")
 
         menu.add_command(
             label="Abrir",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.abrirarchivo
         )
         menu.add_command(
             label="Guardar",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         menu.add_command(
             label="Guardar como",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
 
         sub_menu_archivo_exportar = tk.Menu(menu, tearoff=False)
         sub_menu_archivo_exportar.add_command(
             label="Excel",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         sub_menu_archivo_exportar.add_command(
             label="Imagen",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         sub_menu_archivo_exportar.add_command(
             label="PDF",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         menu.add_cascade(menu=sub_menu_archivo_exportar, label="Exportar")
 
         menu.add_command(
             label="Importar datos",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         menu.add_command(
             label="Inicio",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         menu.add_command(
             label="Imprimir",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
 
@@ -353,23 +358,23 @@ class VentanaPrincipal(tk.Tk):
     def menueditar(self, menu, bar_menu):
         menu.add_command(
             label="Deshacer",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         sub_menu_editar_nodo = tk.Menu(menu, tearoff=False)
         sub_menu_editar_nodo.add_command(
             label="Agregar",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         sub_menu_editar_nodo.add_command(
             label="Editar",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         sub_menu_editar_nodo.add_command(
             label="Eliminar",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         menu.add_cascade(menu=sub_menu_editar_nodo, label="Nodo")
@@ -377,17 +382,17 @@ class VentanaPrincipal(tk.Tk):
         sub_menu_editar_arco = tk.Menu(menu, tearoff=False)
         sub_menu_editar_arco.add_command(
             label="Agregar",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         sub_menu_editar_arco.add_command(
             label="Editar",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         sub_menu_editar_arco.add_command(
             label="Eliminar",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         menu.add_cascade(menu=sub_menu_editar_arco, label="Arco")
@@ -398,22 +403,22 @@ class VentanaPrincipal(tk.Tk):
         sub_menu_analizar_algoritmo = tk.Menu(menu, tearoff=False)
         sub_menu_analizar_algoritmo.add_command(
             label="Dikstra",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=lambda: abrir_ventana_dijkstra(self)
         )
         sub_menu_analizar_algoritmo.add_command(
             label="Algoritmo 2",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         sub_menu_analizar_algoritmo.add_command(
             label="Algoritmo 3",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         sub_menu_analizar_algoritmo.add_command(
             label="Algoritmo k",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         menu.add_cascade(menu=sub_menu_analizar_algoritmo, label="Algoritmo")
@@ -423,7 +428,7 @@ class VentanaPrincipal(tk.Tk):
     def menuherramienta(self, menu, bar_menu):
         menu.add_command(
             label="Ejecución",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         return (menu, bar_menu)
@@ -432,22 +437,22 @@ class VentanaPrincipal(tk.Tk):
         sub_menu_aplicacion = tk.Menu(menu, tearoff=False)
         sub_menu_aplicacion.add_command(
             label="Aplicación 1",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         sub_menu_aplicacion.add_command(
             label="Aplicación 2",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         sub_menu_aplicacion.add_command(
             label="Aplicación 3",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         sub_menu_aplicacion.add_command(
             label="Aplicación m",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         menu.add_cascade(menu=sub_menu_aplicacion, label="Algoritmo")
@@ -457,12 +462,12 @@ class VentanaPrincipal(tk.Tk):
     def menuventana(self, menu, bar_menu):
         menu.add_command(
             label="Gráfica",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         menu.add_command(
             label="Tabla",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         return (menu, bar_menu)
@@ -470,12 +475,12 @@ class VentanaPrincipal(tk.Tk):
     def menuayuda(self, menu, bar_menu):
         menu.add_command(
             label="Ayuda",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         menu.add_command(
             label="Acerca de Grafos",
-            ## accelerator="Ctrl+N",
+            # accelerator="Ctrl+N",
             command=self.archivo_nuevo_presionado
         )
         return (menu, bar_menu)
@@ -485,8 +490,8 @@ class ventanadikstra(tk.Toplevel):
     # secundaria está en uso.
     en_uso = False
 
-    def __init__(self, *args, **kwargs):  ## Queda abierto a n argumentos o n argumentos con identificador
-        super().__init__(*args, **kwargs)  ## Se almacena por herencia el *args **kwargs
+    def __init__(self, *args, **kwargs):  # Queda abierto a n argumentos o n argumentos con identificador
+        super().__init__(*args, **kwargs)  # Se almacena por herencia el *args **kwargs
 
         self.frameDijkstra = tk.Frame(self)
         self.geometry("800x400")
--- menus.py
+++ menus.py
@@ -2,21 +2,32 @@ import tkinter as tk
 class MyDialog(tk.simpledialog.Dialog):
 
     def __init__(self, parent, title):
+        self.ok_button = None
+        self.MultiGraphDir = None
+        self.MultiGraph = None
+        self.GraphNormal = None
+        self.GraphDirigido = None
+        self.Title_label = None
+        self.choseOption = None
+        self.var = None
         self.my_option = None
         super().__init__(parent, title)
     def body(self, frame):
         self.var = tk.StringVar()
         self.choseOption = ["Grafo", "Grafo Dirigido", "Multi Grafo", "Multi Grafo Dirigido"]
         self.var.set(self.choseOption[0])
-        self.Title_label = tk.Label(frame, width=25, text="Seleccione el tipo de grafo a crear")
+        self.Title_label = tk.Label(frame, width=28, text="Seleccione el tipo de grafo a crear", font=("Segoe UI", 12))
         self.Title_label.grid(row=0, rowspan=2, column=0, pady=15)
-        self.GraphNormal = tk.Radiobutton(frame, text="Grafo", variable=self.var, value="Grafo")
+        self.GraphNormal = tk.Radiobutton(frame, text="Grafo", variable=self.var, value="Grafo", font=("Segoe UI", 9))
         self.GraphNormal.grid(row=1, rowspan=2, column=0, pady=15)
-        self.GraphDirigido = tk.Radiobutton(frame, text="Grafo Dirigido", variable=self.var, value="Grafo Dirigido")
+        self.GraphDirigido = tk.Radiobutton(frame, text="Grafo Dirigido", variable=self.var, value="Grafo Dirigido",
+                                            font=("Segoe UI", 9))
         self.GraphDirigido.grid(row=2, rowspan=2, column=0, pady=15)
-        self.MultiGraph = tk.Radiobutton(frame, text="Multi Grafo", variable=self.var, value="Multi Grafo")
+        self.MultiGraph = tk.Radiobutton(frame, text="Multi Grafo", variable=self.var, value="Multi Grafo",
+                                         font=("Segoe UI", 9))
         self.MultiGraph.grid(row=3, rowspan=2, column=0, pady=15)
-        self.MultiGraphDir = tk.Radiobutton(frame, text="Multi Grafo Dirigido", variable=self.var, value="Multi Grafo Dirigido")
+        self.MultiGraphDir = tk.Radiobutton(frame, text="Multi Grafo Dirigido", variable=self.var, 
+                                            value="Multi Grafo Dirigido", font=("Segoe UI", 9))
         self.MultiGraphDir.grid(row=4, rowspan=2, column=0, pady=15)
 
         return frame
@@ -25,6 +36,6 @@ class MyDialog(tk.simpledialog.Dialog):
         self.destroy()
 
     def buttonbox(self):
-        self.ok_button = tk.Button(self, text='OK', width=40, command=self.ok_pressed)
+        self.ok_button = tk.Button(self, text='Crear', width=40, command=self.ok_pressed)
         self.ok_button.pack(fill="both")
         self.bind("<Return>", lambda event: self.ok_pressed())
