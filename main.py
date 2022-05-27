b75dc063ee071c438c05a731de8b708982057332
Your Name <nasuarezro@unal.edu.co>
2022-05-27 02:01:33 -0500

    Update

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 from Ventanas import *
 
 Ventana = VentanaPrincipal()
@@ -7,22 +5,16 @@ Ventana.focus()
 Ventana.init_menubar()
 Ventana.init_buttons()
 
-
-
-
 # Ventana.update()
-
 
 """# Makes the window topmost
 Ventana.wm_attributes('-topmost', True)
-
 
 # Makes the window ubication center, n, ne, nw, etc
 Ventana.anchor("center")
 
 # Makes the icon of the window
 Ventana.wm_iconbitmap("error")
-
 
 
 # Makes the window transparent
@@ -33,11 +25,5 @@ Ventana.wm_attributes('-disabled', True)
 
 # Set the geometry of the window
 Ventana.wm_geometry('700x350')"""
-
-
-
-
-
-
 
 Ventana.mainloop()
